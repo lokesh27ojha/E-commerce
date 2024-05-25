7b0d9580b3915b290696758e996155735af0d4f7
lokesh27ojha <lokesh27ojha@gmail.com>
2024-05-25 13:11:35 +0530

    first commit

---
+++ backend/lootmart/src/main/java/com/example/lootmart/LootmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.lootmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LootmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LootmartApplication.class, args);
+	}
+
+}
